5e2abc77aed44fc88a12899a1ea115c22788ac42
Priyanka Chandrashekar <priyanka.cs@unity3d.com>
2023-05-29 11:48:33 -0700

    working when python is run separately
    to do: run python script from unity and then read its output

--- ScenarioSprintProject/Assets/ImageProcTests/AnalyzeImage.cs
+++ ScenarioSprintProject/Assets/ImageProcTests/AnalyzeImage.cs
@@ -1,15 +1,73 @@
+using System;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Debug = UnityEngine.Debug;
 
 public class AnalyzeImage : MonoBehaviour
 {
     string m_CaptureLocation;
     DirectoryInfo m_Directory;
-    // Start is called before the first frame update
+    
+    
+
+    Thread receiveThread;
+    UdpClient client;
+    int port;
+
+    string lastReceivedUDPPacket = "";
+    string allReceivedUDPPackets = "";
+
+    Vector3 up;
+    bool jump;
+    
     void Start()
     {
         m_CaptureLocation = Application.dataPath + "/ImageProcTests/Screenshots";
         m_Directory = new DirectoryInfo(m_CaptureLocation);
+
+        InitializeUDP();
+    }
+
+    void InitializeUDP()
+    {
+        print ("UPDSend.init()");
+        port = 5065;
+
+        print ("Sending to 127.0.0.1 : " + port);
+
+        receiveThread = new Thread (new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start ();
+
+    }
+
+    void ReceiveData()
+    {
+        client = new UdpClient (port);
+        while (true) 
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port);
+                var data = client.Receive(ref anyIP);
+
+                var text = Encoding.UTF8.GetString(data);
+                Debug.Log("Received: " + text);
+                lastReceivedUDPPacket=text;
+                allReceivedUDPPackets=allReceivedUDPPackets+text;
+
+                break;
+
+            }
+            catch(Exception e){
+                print (e.ToString());
+            }
+        }
     }
 
     // Update is called once per frame
@@ -19,18 +77,13 @@ public class AnalyzeImage : MonoBehaviour
         {
             // Processing images
             
-            Debug.Log(m_CaptureLocation);
-
-            var files = m_Directory.GetFiles("*.png");
-            foreach (var file in files)
-            {
-                Debug.Log(file.Name);
-            }
-            
-            // var baseImage = File.ReadAllBytes(m_CaptureLocation + "base.png");
-            
-            Debug.Log("done");
-
+            Debug.Log("processing images");
         }
     }
+
+	void OnApplicationQuit(){
+		if (receiveThread != null) {
+			receiveThread.Abort();
+		}
+	}
 }
