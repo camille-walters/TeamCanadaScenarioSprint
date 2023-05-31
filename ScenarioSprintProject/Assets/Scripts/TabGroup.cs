2bd5de9fd503e78179e34e61bd2a5bb50ff11f6b
Camille Walters <camille.h.walters@gmail.com>
2023-05-31 12:52:42 -0400

    UI stuffs

---
+++ ScenarioSprintProject/Assets/Scripts/TabGroup.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabClick> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+    // Start is called before the first frame update
+    public void Subscribe(TabClick button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabClick>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabClick button)
+    {
+        Debug.Log("here");
+        ResetTabs();
+        button.background.color = tabHover;
+    }
+
+    public void OnTabExit(TabClick button)
+    {
+        ResetTabs();
+        button.background.color = tabIdle;
+    }
+
+    public void OnTabSelected(TabClick button)
+    {
+        ResetTabs();
+        button.background.color = tabActive;
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabClick button in tabButtons)
+        {
+            button.background.color = tabIdle;
+        }
+    }
+}
