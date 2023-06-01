63ab267aecb1f5bebbe04b7ee8eb82ea3e6efe1b
Camille Walters <camille.h.walters@gmail.com>
2023-06-01 14:34:49 -0400

    UITest initial layout

--- ScenarioSprintProject/Assets/Scripts/TabClick.cs
+++ ScenarioSprintProject/Assets/Scripts/TabClick.cs
@@ -26,7 +26,7 @@ public class TabClick : MonoBehaviour, IPointerEnterHandler, IPointerClickHandle
     }
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         background = GetComponent<Image>();
         tabGroup.Subscribe(this);
