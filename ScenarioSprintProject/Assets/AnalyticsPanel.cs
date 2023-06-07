69cc4935a5e162a5a5619e7665411fea3bc621c1
Camille Walters <camille.h.walters@gmail.com>
2023-06-07 17:10:13 -0400

    Defects with dummy data is working

--- ScenarioSprintProject/Assets/AnalyticsPanel.cs
+++ ScenarioSprintProject/Assets/AnalyticsPanel.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AnalyticsPanel : MonoBehaviour
+//updates Panel with Data from AnalyticsData
+public class AnalyticsPanel : MonoBehaviour 
 {
+    public TMP_Dropdown dropdown;
+    List<string> dropdownOptions = new List<string>() { "Throughput over Time", "Defects over Time" };
+
+    public TMP_Text totalDefects;
+    public TMP_Text majorDefects;
+    public TMP_Text minorDefects;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PopulateDropdownList();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //probably doesnt need to be done in update
+        totalDefects.text = AnalyticsData.Instance.totalDefects.ToString();
+        majorDefects.text = AnalyticsData.Instance.majorDefects.ToString();
+        minorDefects.text = AnalyticsData.Instance.minorDefects.ToString();
+    }
+
+    public void DropdownIndexChanged(int index)
+    {
+        if (index == 0)
+        {
+            Debug.Log("showing throughput");//TODO: replace this accordingly
+        }
+        else if (index == 1)
+        {
+            Debug.Log("showing defects");//TODO: replace accordingly
+        }
+        else return;
+    }
+    void PopulateDropdownList()
+    {
+        dropdown.AddOptions(dropdownOptions);
     }
 }
