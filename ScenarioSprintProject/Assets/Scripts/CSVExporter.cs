c218bb82061648fd195a85c96d104abdf9a5b3cb
Priyanka Chandrashekar <priyanka.cs@unity3d.com>
2023-06-09 13:12:39 -0700

    working input export

--- ScenarioSprintProject/Assets/Scripts/CSVExporter.cs
+++ ScenarioSprintProject/Assets/Scripts/CSVExporter.cs
@@ -7,14 +7,20 @@ using UnityEngine;
 public class CSVExporter : MonoBehaviour
 {
     public string csvPath;
-    public ControlPanel input;
-    public AnalyticsData output;
+    public float timeInterval = 10;
+    ControlPanel m_Input;
+    AnalyticsData m_Output;
     DateTime m_StartTime;
 
     void Awake()
     {
-        csvPath = Application.dataPath + "/SimulationData.csv";
+        if (csvPath == "")
+            csvPath = Application.dataPath + "/SimulationData.csv";
         m_StartTime = DateTime.Now;
+        m_Input = FindObjectOfType<ControlPanel>();
+        m_Output = FindObjectOfType<AnalyticsData>();
+
+        m_Input.SetTimeInterval(timeInterval);
     }
 
     public void OnExportButtonClick()
@@ -27,25 +33,45 @@ public class CSVExporter : MonoBehaviour
 
                 var count = new List<int>
                 {
-                    output.minorDefectsList.Count,
-                    output.majorDefectsList.Count,
-                    output.totalDefectsList.Count,
-                    output.throughPutOverTimeList.Count,
-                    output.throughPutOverCarList.Count,
-                    output.workerUtilizationList.Count
+                    m_Input.conveyorSpeedList.Count,
+                    m_Input.sprayRadiusList.Count,
+                    m_Input.sprayAngleList.Count,
+                    m_Input.sprayPressureList.Count,
+                    m_Input.distanceFromCarList.Count,
+                    m_Input.movementSpeedList.Count,
+                    m_Input.numberOfWorkersList.Count,
+                    m_Input.timeToFixMinorDefectsList.Count,
+                    m_Input.timeToFixMajorDefectsList.Count,
+                    m_Output.minorDefectsList.Count,
+                    m_Output.majorDefectsList.Count,
+                    m_Output.totalDefectsList.Count,
+                    m_Output.throughPutOverTimeList.Count,
+                    m_Output.throughPutOverCarList.Count,
+                    m_Output.workerUtilizationList.Count
                 }.Min();
                 
-                writer.WriteLine("time,minorDefects,majorDefects,totalDefects,throughPutOverTime,throughPutOverCar,workerUtilization");
+                writer.WriteLine("time," + 
+                    "conveyorSpeed,sprayRadius,sprayAngle,sprayPressure,distanceFromCar,movementSpeed,numberOfWorkers,timeToFixMinorDefects,timeToFixMajorDefects," +
+                    "minorDefects,majorDefects,totalDefects,throughPutOverTime,throughPutOverCar,workerUtilization");
                 for (var i = 0; i < count; ++i)
                 {
                     var rowTime = m_StartTime.AddSeconds(i * 10);
-                    writer.WriteLine(rowTime.ToString("HH:mm:ss") + "," + 
-                        output.minorDefectsList[i] + "," + 
-                        output.majorDefectsList[i] + "," + 
-                        output.totalDefectsList[i] + "," + 
-                        output.throughPutOverTimeList[i] + "," + 
-                        output.throughPutOverCarList[i] + "," + 
-                        output.workerUtilizationList[i] + ",");
+                    writer.WriteLine(rowTime.ToString("HH:mm:ss") + "," +
+                        m_Input.conveyorSpeedList[i].ToString("0.###") + "," + 
+                        m_Input.sprayRadiusList[i].ToString("0.###") + "," + 
+                        m_Input.sprayAngleList[i].ToString("0.###") + "," + 
+                        m_Input.sprayPressureList[i].ToString("0.###") + "," + 
+                        m_Input.distanceFromCarList[i].ToString("0.###") + "," + 
+                        m_Input.movementSpeedList[i].ToString("0.###") + "," + 
+                        m_Input.numberOfWorkersList[i] + "," + 
+                        m_Input.timeToFixMinorDefectsList[i].ToString("0.###") + "," + 
+                        m_Input.timeToFixMajorDefectsList[i].ToString("0.###") + "," +
+                        m_Output.minorDefectsList[i].ToString("0.###") + "," + 
+                        m_Output.majorDefectsList[i].ToString("0.###") + "," + 
+                        m_Output.totalDefectsList[i].ToString("0.###") + "," + 
+                        m_Output.throughPutOverTimeList[i].ToString("0.###") + "," + 
+                        m_Output.throughPutOverCarList[i].ToString("0.###") + "," + 
+                        m_Output.workerUtilizationList[i].ToString("0.###") + ",");
                 }
 
                 writer.Flush();
@@ -55,7 +81,7 @@ public class CSVExporter : MonoBehaviour
         }
         catch(Exception exp)  
         {  
-            Debug.Log(exp.Message);  
+            Debug.LogWarning("ERROR:" + exp.Message);  
         }
     }
 }
