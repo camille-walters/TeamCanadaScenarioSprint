56838b2a85dbf6a5f42612947a0145942fbf5157
Priyanka Chandrashekar <priyanka.cs@unity3d.com>
2023-06-13 10:09:38 -0700

    defects per car

--- Scripts/post-simulation-analysis.py
+++ Scripts/post-simulation-analysis.py
@@ -42,6 +42,22 @@ for f in files:
         times[index] = times[index].seconds
         index += 1
     sim[0] = times
+
+    # add final row with number of defects per car
+    defects_per_car = [x/y if y else 0 for x, y in zip(sim[12], sim[16])]
+    sim.append(defects_per_car)
+    '''
+    defects_per_car = sim[12]
+    for x in range(0, len(defects_per_car)):
+        if sim[16][i] != 0:
+            defects_per_car[i] = defects_per_car[i]/sim[16][i]
+        else:
+            defects_per_car[i] =0
+    sim.append(defects_per_car)
+    '''
+    print(defects_per_car)
+
+
     sims.append(sim)
 
 # Get minimum number of values for uniformity across graphs
@@ -66,33 +82,42 @@ fig.write_html('sims.html', auto_open=True)
 fig_total_cars_processed = make_subplots(specs=[[{"secondary_y": True}]])
 fig_time_to_process_car = make_subplots(specs=[[{"secondary_y": True}]])
 fig_defects = make_subplots(specs=[[{"secondary_y": True}]])
+fig_defects_per_car = make_subplots(specs=[[{"secondary_y": True}]])
 fig_worker_utilization = make_subplots(specs=[[{"secondary_y": True}]])
 
 for i in range(0, len(sims)):
     trace_total_cars_processed = go.Line(x=sims[i][0][:count], y=sims[i][16][:count], name="Simulation" + str(i))
     trace_total_time_to_process_cars = go.Line(x=sims[i][0][:count], y=sims[i][14][:count], name="Simulation" + str(i))
     trace_total_defects = go.Line(x=sims[i][0][:count], y=sims[i][12][:count], name="Simulation" + str(i))
+    trace_total_defects_per_car = go.Line(x=sims[i][0][:count], y=sims[i][17][:count], name="Simulation" + str(i))
     trace_worker_utilization = go.Line(x=sims[i][0][:count], y=sims[i][15][:count], name="Simulation" + str(i))
 
     if i == 0:
         fig_total_cars_processed.add_trace(trace_total_cars_processed)
         fig_time_to_process_car.add_trace(trace_total_time_to_process_cars)
         fig_defects.add_trace(trace_total_defects)
+        fig_defects_per_car.add_trace(trace_total_defects_per_car)
         fig_worker_utilization.add_trace(trace_worker_utilization)
     else:
-        fig_total_cars_processed.add_trace(trace_total_cars_processed, secondary_y=True)
-        fig_time_to_process_car.add_trace(trace_total_time_to_process_cars, secondary_y=True)
-        fig_defects.add_trace(trace_total_defects, secondary_y=True)
-        fig_worker_utilization.add_trace(trace_worker_utilization, secondary_y=True)
+        fig_total_cars_processed.add_trace(trace_total_cars_processed)  #, secondary_y=True)
+        fig_time_to_process_car.add_trace(trace_total_time_to_process_cars)  #, secondary_y=True)
+        fig_defects.add_trace(trace_total_defects)  #, secondary_y=True)
+        fig_defects_per_car.add_trace(trace_total_defects_per_car)  #, secondary_y=True)
+        fig_worker_utilization.add_trace(trace_worker_utilization)  #, secondary_y=True)
 
-fig_total_cars_processed.update_layout(title_text="Total Cars Processed")
+fig_total_cars_processed.update_layout(yaxis_title="Number of cars processed", xaxis_title="Time (seconds)", title_text="Total Cars Processed")
 fig_total_cars_processed.write_html('total_cars_processed.html', auto_open=True)
 
-fig_time_to_process_car.update_layout(title_text="Time to process each car")
+'''
+fig_time_to_process_car.update_layout(yaxis_title="Time to process each car", xaxis_title="Time (seconds)", title_text="Time to process each car")
 fig_time_to_process_car.write_html('time_to_process_car.html', auto_open=True)
+'''
 
-fig_defects.update_layout(title_text="Total Defects")
+fig_defects.update_layout(yaxis_title="Number of defects", xaxis_title="Time (seconds)", title_text="Total Defects")
 fig_defects.write_html('total_defects.html', auto_open=True)
 
-fig_worker_utilization.update_layout(title_text="Worker Utilization")
+fig_defects_per_car.update_layout(yaxis_title="Number of defects per car", xaxis_title="Time (seconds)", title_text="Defects per Car")
+fig_defects_per_car.write_html('total_defects_per_car.html', auto_open=True)
+
+fig_worker_utilization.update_layout(yaxis_title="Worker utilization", xaxis_title="Time (seconds)", title_text="Worker Utilization")
 fig_worker_utilization.write_html('worker_utilization.html', auto_open=True)
